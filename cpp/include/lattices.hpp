4d263b6a6effb72a2af61f9a4c02db68aa74e74f
David Jansen <jansen.david0712@gmail.com>
2025-04-30 19:05:12 +0200

    made substantial refactoring of code, added 1d and tfi model

---
+++ cpp/include/lattices.hpp
@@ -0,0 +1,306 @@
+#pragma once
+#include "fusion.h"
+#include "spins.hpp"
+#include <unordered_map>
+#include <memory>
+#include "symmetries.hpp"
+using namespace mosek::fusion;
+using namespace monty;
+using int_pair = std::pair<int, int>;
+using TI_map_type = std::map<std::string, std::pair<std::string, std::complex<double>>>;
+class LatticeBase
+{
+public:
+	LatticeBase(int Ly, int Lx) : Ly_(Ly), Lx_(Lx) {};
+	int Ly_;
+	int Lx_;
+	TI_map_type TI_map_;
+	void generate_TI_map(std::map<std::string, op_vec> &mat_terms, std::vector<op_vec> &operators_, int sign_sector_) {};
+	struct G_el
+	{
+		double prefac_;
+		int pos_;
+		G_el(double prefac, int pos) : prefac_(prefac), pos_(pos) {};
+	};
+
+	struct G_op
+	{
+		std::complex<double> prefac_;
+		std::string op_;
+		G_op(std::complex<double> prefac, std::string op) : prefac_(prefac), op_(op) {};
+	};
+
+	G_op generate_G_element_sos(op_vec op1, op_vec op2, int j, int i, TI_map_type &TI_map_)
+	{
+		// Generate all elements of the first row with translation in y direction. j go in y direction
+
+		auto op_dagg_first = dagger_operator(op1);
+
+		auto [fac_dagg, op_dagger] = get_normal_form(op_dagg_first);
+
+		assert(std::abs(fac_dagg.imag()) < 1e-9);
+		op_vec new_op_y;
+		op_vec new_op;
+		if (j > 0)
+		{
+			new_op_y = translation_y(op2, j, Ly_);
+		}
+		else
+		{
+
+			new_op_y = op2;
+		}
+		if (i > 0)
+		{
+			new_op = translation(new_op_y, i, Lx_);
+		}
+		else
+		{
+			new_op = new_op_y;
+		}
+		auto v_x = op_dagger;
+
+		v_x.insert(v_x.end(), new_op.begin(), new_op.end());
+		auto [fac, vec] = get_normal_form(v_x);
+
+		auto [ti_key, ti_val] = TI_map_.at(print_op(vec));
+
+		cpx total_fac = fac; // fac*ti_val;
+		return G_op(total_fac, ti_key);
+	}
+};
+
+class SquareLattice : public LatticeBase
+{
+public:
+	std::string permuts_;
+
+	bool bilayer_;
+	bool square_;
+	std::vector<int> get_offset_vec()
+	{
+		if (bilayer_)
+		{
+			return {2, Ly_, Lx_};
+		}
+		else
+		{
+			return {Ly_, Lx_};
+		}
+	}
+
+	SquareLattice(int Ly, int Lx, bool square, bool bilayer, std::string permuts = "xyz") : LatticeBase(Ly, Lx), bilayer_(bilayer), square_(square), permuts_(permuts)
+	{
+		// assert(Lx == Ly);
+		if (permuts != "xyz" and permuts != "xy")
+		{
+			std::cout << "permutation error" << std::endl;
+		}
+	};
+	template <typename container>
+	std::pair<bool, std::string> check_if_operator_exists(op_vec op, container &mat_terms, bool rdm_check = false)
+	{
+		// rdm_check check if exist, if not print something
+		//  check if the operator is contained in functions
+		// returs if_found, and where
+		if (op.size() < 1)
+		{
+
+			auto it = mat_terms.find(print_op(op));
+			if (it != mat_terms.end())
+			{
+				return {true, print_op(op)};
+			}
+			else
+			{
+				return {false, print_op(op)};
+			}
+		}
+
+		if (is_zero_signsym(op))
+		{
+
+			if (mat_terms.find("0") != mat_terms.end())
+			{
+
+				return {true, "0"};
+			}
+			else
+			{
+
+				return {false, "0"};
+			}
+		}
+
+		auto all_ty = generate_all_translations_y(op, Ly_, 1);
+
+		bool found = false;
+
+		for (auto op_ty : all_ty)
+		{
+			auto all_t = generate_all_translations(op_ty, Lx_);
+
+			for (auto op_t : all_t)
+			{
+				auto it = mat_terms.find(print_op(op_t));
+				if (it != mat_terms.end())
+				{
+
+					// TI_map_.insert({print_op(op), {it->first, 1}});
+					return {true, it->first};
+				}
+				std::vector<op_vec> all_p;
+				if (permuts_ == "xyz" or permuts_ == "yxz" or permuts_ == "zxy" or permuts_ == "zyx")
+				{
+					all_p = generate_all_permutations_xyz(op_t);
+				}
+				else if (permuts_ == "xy")
+				{
+					all_p = generate_all_permutations_xy(op_t);
+				}
+				for (auto op_p : all_p)
+				{
+					if (!square_)
+					{
+						auto it = mat_terms.find(print_op(op_p));
+						if (it != mat_terms.end())
+						{
+
+							return {true, it->first};
+						}
+					}
+					else
+					{
+						auto all_d8sym = generate_all_d8(op_p, Lx_);
+
+						for (auto d8s : all_d8sym)
+						{
+							auto it = mat_terms.find(print_op(d8s));
+							if (it != mat_terms.end())
+							{
+
+								return {true, it->first};
+							}
+
+							auto op_mirror = mirror(d8s);
+
+							it = mat_terms.find(print_op(op_mirror));
+							if (it != mat_terms.end())
+							{
+
+								return {true, it->first};
+							}
+							if (bilayer_)
+							{
+								auto op_flip_layer = flip_layer(d8s);
+								it = mat_terms.find(print_op(op_flip_layer));
+								if (it != mat_terms.end())
+								{
+
+									return {true, it->first};
+								}
+								op_flip_layer = flip_layer(op_mirror);
+								it = mat_terms.find(print_op(op_flip_layer));
+								if (it != mat_terms.end())
+								{
+
+									return {true, it->first};
+								}
+							}
+						}
+					}
+				}
+			}
+		}
+		if (rdm_check)
+		{
+
+			std::cout << print_op(op) << " in rdm did not exist" << std::endl;
+			assert(false);
+		}
+		return {false, print_op(op)};
+	}
+	void generate_TI_map(std::map<std::string, op_vec> &mat_terms, std::vector<op_vec> &operators_, int sign_sector)
+	{
+		//     // generate all elemenets with translation symmetrie in x and y direction
+
+		int index = 0;
+		for (auto it1 = operators_.begin(); it1 != operators_.end(); ++it1)
+		{
+			auto [fac, vec] = get_normal_form(*it1);
+
+			auto [found, op_string] = check_if_operator_exists(vec, mat_terms);
+			if (found)
+			{
+				TI_map_.insert({print_op(vec), {op_string, 1}});
+			}
+			else
+			{
+				// std::cout << "called" << std::endl;
+				mat_terms.insert({print_op(vec), vec});
+				TI_map_.insert({print_op(vec), {print_op(vec), 1}});
+			}
+
+			// diagonal elements
+
+			for (auto it2 = it1; it2 != operators_.end(); ++it2)
+			{
+
+				// if(it2!=it1)
+				{
+					// std::cout<<"xx"<<std::endl;
+					auto op_cp = *it2;
+
+					for (int n = 0; n < Ly_; n++)
+					{
+						for (int m = 0; m < Lx_; m++)
+						{
+							op_vec new_op_y;
+							op_vec new_op;
+							if (n > 0)
+							{
+
+								new_op_y = translation_y(op_cp, n, Ly_);
+							}
+							else
+							{
+								new_op_y = op_cp;
+							}
+
+							if (m > 0)
+							{
+
+								new_op = translation(new_op_y, m, Lx_);
+							}
+							else
+							{
+								new_op = new_op_y;
+							}
+
+							auto op_dagger = dagger_operator(*it1);
+							auto [fac, v_x] = get_normal_form(op_dagger);
+							assert(std::abs(fac.imag() < 1e-9));
+
+							v_x.insert(v_x.end(), new_op.begin(), new_op.end());
+
+							auto [fac_tot, vec_tot] = get_normal_form(v_x);
+
+							auto [found, op_string] = check_if_operator_exists(vec_tot, mat_terms);
+							if (found)
+							{
+								TI_map_.insert({print_op(vec_tot), {op_string, 1}});
+							}
+							else
+							{
+								mat_terms.insert({op_string, vec_tot});
+								TI_map_.insert({op_string, {op_string, 1}});
+							}
+						}
+					}
+				}
+			}
+		}
+
+		return;
+	}
+};
--- cpp/include/spins.hpp
+++ cpp/include/spins.hpp
@@ -1,307 +1,281 @@
 #pragma once
-#include"vector"
-#include"string"
+#include "vector"
+#include "string"
 #include <iostream>
-using cpx =std::complex<double>;
+using cpx = std::complex<double>;
 
-class spin_op{
+class spin_op
+{
 public:
   std::string dir_;
   std::vector<int> site_;
 
   std::vector<int> offset_;
-  bool unit=false;
-  spin_op(std::string dir, std::vector<int> site, std::vector<int> offset):dir_(dir), site_(site), offset_(offset){
-    if(site.size()<offset.size()){std::cout<< "Need sufficient offsets"<<std::endl;}};
-  spin_op(){ unit=true;};
-  int const pos()const {
-    
-    int position=0;
-    for(int i=site_.size()-1; i>-1; i--)
-      {
-       // std::cout<< "offset "<<offset_[i]<< " and factor "<<offset_.size()-1<<std::endl;
-	    position+=site_[i]*std::pow(offset_[i],i);
-      }
-     
-      return position;
+  bool unit = false;
+  spin_op(std::string dir, std::vector<int> site, std::vector<int> offset) : dir_(dir), site_(site), offset_(offset)
+  {
+    if (site.size() < offset.size())
+    {
+      std::cout << "Need sufficient offsets" << std::endl;
+    }
+  };
+  spin_op() { unit = true; };
+  int const pos() const
+  {
 
+    int position = 0;
+    for (int i = site_.size() - 1; i > -1; i--)
+    {
+      // std::cout<< "offset "<<offset_[i]<< " and factor "<<offset_.size()-1<<std::endl;
+      position += site_[i] * std::pow(offset_[i], i);
+    }
+
+    return position;
   }
-    spin_op get_mirror()
+  spin_op get_mirror()
   {
     // assumes LxL lattice and order ....y,x
-    auto new_sites=site_;
+    auto new_sites = site_;
 
-    new_sites[new_sites.size()-1]=site_[site_.size()-2];
-    new_sites[new_sites.size()-2]=site_[site_.size()-1];
-    
-    return spin_op(dir_,new_sites, offset_ );
+    new_sites[new_sites.size() - 1] = site_[site_.size() - 2];
+    new_sites[new_sites.size() - 2] = site_[site_.size() - 1];
+
+    return spin_op(dir_, new_sites, offset_);
   }
-     spin_op get_flipped_layer()
+  spin_op get_flipped_layer()
   {
     // assumes LxL lattice and order ....y,x
-    assert(site_.size()==3);
-    auto new_sites=site_;
+    assert(site_.size() == 3);
+    auto new_sites = site_;
 
-    new_sites[0]=(site_[0]+1)%2;
- 
-    return spin_op(dir_,new_sites, offset_ );
+    new_sites[0] = (site_[0] + 1) % 2;
+
+    return spin_op(dir_, new_sites, offset_);
   }
- spin_op get_translated(int j, int L)
+  spin_op get_translated(int j, int L)
   {
-    auto new_sites=site_;
-    new_sites[new_sites.size()-1]=(new_sites[new_sites.size()-1]+j)%L;
-    
-    return spin_op(dir_,new_sites, offset_ );
+    auto new_sites = site_;
+    new_sites[new_sites.size() - 1] = (new_sites[new_sites.size() - 1] + j) % L;
+
+    return spin_op(dir_, new_sites, offset_);
   }
 
-   spin_op get_translated_y(int j, int L)
+  spin_op get_translated_y(int j, int L)
   {
-    auto new_sites=site_;
-    
-    new_sites[new_sites.size()-2]=(new_sites[new_sites.size()-2]+j)%L;
-    
-    return spin_op(dir_,new_sites, offset_ );
+    auto new_sites = site_;
+
+    new_sites[new_sites.size() - 2] = (new_sites[new_sites.size() - 2] + j) % L;
+
+    return spin_op(dir_, new_sites, offset_);
   }
-  
+
   std::string expression() const
   {
-    std::string exp="s";
+    std::string exp = "s";
 
-    exp+="_[";
-    
-    exp+=dir_+",(";
-    for(int i=0; i< site_.size()-1;i++)
+    exp += "_[";
+
+    exp += dir_ + ",(";
+    for (int i = 0; i < site_.size() - 1; i++)
     {
-      exp+=std::to_string(site_[i])+",";
+      exp += std::to_string(site_[i]) + ",";
     }
-      exp+=std::to_string(site_[site_.size()-1]);
-  exp+=")]";
-  return exp;
-}
-   bool operator<(const spin_op& obj) const
-    { 
+    exp += std::to_string(site_[site_.size() - 1]);
+    exp += ")]";
+    return exp;
+  }
+  bool operator<(const spin_op &obj) const
+  {
 
-          
-	    return this->expression()<obj.expression();
-	  
-    } 
-   bool operator>(const spin_op& obj) const
-    { 
-	    return this->expression()>obj.expression();        
+    return this->expression() < obj.expression();
+  }
+  bool operator>(const spin_op &obj) const
+  {
+    return this->expression() > obj.expression();
+  }
+  bool operator==(const spin_op &obj)
+  {
 
-    } 
-bool operator == (const spin_op& obj)
-{
-   
-  return (expression() == obj.expression());
-}
-  
-    friend bool operator== (const spin_op& c1, const spin_op& c2);
-    friend bool operator!= (const spin_op& c1, const spin_op& c2);
+    return (expression() == obj.expression());
+  }
+
+  friend bool operator==(const spin_op &c1, const spin_op &c2);
+  friend bool operator!=(const spin_op &c1, const spin_op &c2);
 };
-bool operator== (const spin_op& c1, const spin_op& c2)
+bool operator==(const spin_op &c1, const spin_op &c2)
 {
-    return (c1.expression() == c2.expression());
+  return (c1.expression() == c2.expression());
 }
 
-bool operator!= (const spin_op& c1, const spin_op& c2)
+bool operator!=(const spin_op &c1, const spin_op &c2)
 {
-    return (c1.expression() != c2.expression());
+  return (c1.expression() != c2.expression());
 }
 
-std::ostream& operator<<(std::ostream& os, const spin_op& op)
+std::ostream &operator<<(std::ostream &os, const spin_op &op)
 {
   os << op.expression();
-    return os;
+  return os;
 }
-
 
 /////////////////////////////////////////////////////////////////////////
 using op_vec = std::vector<spin_op>;
-using basis_structure =std::map<int, std::vector<op_vec>>;
+using basis_structure = std::map<int, std::vector<op_vec>>;
 /////////////////////////////////////////////////////////////////
-std::string print_op(const op_vec& oper)
+std::string print_op(const op_vec &oper)
 {
-  std::string s="";
-  if(oper.size()<1)
-    {s+="1";}
-  else{
-  for(auto& O : oper)
+  std::string s = "";
+  if (oper.size() < 1)
+  {
+    s += "1";
+  }
+  else
+  {
+    for (auto &O : oper)
     {
-      s+=O.expression();
-    }}
+      s += O.expression();
+    }
+  }
   return s;
 }
 
 op_vec dagger_operator(op_vec oper)
 {
- double coeff=1;
+  double coeff = 1;
   op_vec new_op;
-  reverse(oper.begin(),oper.end()); 
- return oper;
-
+  reverse(oper.begin(), oper.end());
+  return oper;
 }
-template<typename T>
-T apply_commutator(T& arr, int i)
+template <typename T>
+T apply_commutator(T &arr, int i)
 {
   // if empty vector, then nothing happens (already c^dagc)
   // else change arr and return with deleted element
-  
+
   T arr_copy;
-    copy(arr.begin(), arr.begin()+i, back_inserter(arr_copy));
- 
-  copy(arr.begin()+i+2, arr.end(), back_inserter(arr_copy));   
-      std::swap(arr[i], arr[i + 1]);
-      return arr_copy;
+  copy(arr.begin(), arr.begin() + i, back_inserter(arr_copy));
+
+  copy(arr.begin() + i + 2, arr.end(), back_inserter(arr_copy));
+  std::swap(arr[i], arr[i + 1]);
+  return arr_copy;
 }
 
-
-std::pair<cpx,std::string>get_dir(std::string a, std::string b)
+std::pair<cpx, std::string> get_dir(std::string a, std::string b)
 {
-  std::pair<cpx, std::string> p1(cpx(0,0), std::string("0"));
-  if(a=="x" and b=="y")
-    {return {cpx(0,1), std::string("z")};}
-  else if(a=="y" and b=="x")
-    {return {cpx(0,-1), std::string("z")};}
-  else if(a=="z" and b=="x")
-    {return {cpx(0,1), std::string("y")};}
-  else if(a=="x" and b=="z")
-    {return {cpx(0,-1), std::string("y")};}
-  else if(a=="y" and b=="z")
-    {return {cpx(0,1), std::string("x")};}
-   else if(a=="z" and b=="y")
-     {return {cpx(0,-1), std::string("x")};}
-   else{
-    std::cout<< "error commutators "<<std::endl;
-    return {cpx(0,0), std::string("0")};
-      }
-}
-std::pair<cpx,op_vec> run_loop(op_vec op)
-{
-  cpx pref=1;
-  std::map<int, spin_op> copied;
-    for(int i=0; i<op.size(); i++)
-      {
-	copied.insert({i, op[i]});
-      }
-  for(int i=0; i<op.size()-1; i++)
-    {
-      auto it = copied.find(i);
-
-    if (it != copied.end()) {
-      auto o1=copied[i];
-      auto o2=copied[i+1];
-      if(o1.pos()!=o2.pos())
-	{
-	  continue;
-	}
-      else if(o1.dir_==o2.dir_){copied.erase(i);copied.erase(i+1);}
-      else{
-        auto [new_coeff, new_op]=get_dir(o1.dir_,o2.dir_);
-	copied.erase(i);
-	copied.erase(i+1);
-	copied.insert({i, spin_op(new_op,o1.site_, o1.offset_)});
-	pref*=new_coeff;
-
-      }
-      }
-
-    }
-  
-    op_vec final_vec;
-    for(auto it=copied.begin(); it!=copied.end(); ++it)
-      {
-	final_vec.push_back(it->second);
-      }
-    
-    return {pref, final_vec};
-    
- }
-  
-std::pair<cpx,op_vec> get_normal_form(op_vec op)
-{
-  if(op.size()<1){std::cout<< "error: unit sent to normal form"<<std::endl;}
-
-  cpx pref(1.,0);
-  bool change=false;
-  auto new_list=op;  
-  sort(new_list.begin(), new_list.end(),[](spin_op& o1, spin_op& o2){return o1.pos()<o2.pos();} );
-  for(auto a: new_list)
+  std::pair<cpx, std::string> p1(cpx(0, 0), std::string("0"));
+  if (a == "x" and b == "y")
   {
-    //std::cout<<a.pos()<<std::endl;
+    return {cpx(0, 1), std::string("z")};
   }
-  
-  while(not change)
+  else if (a == "y" and b == "x")
+  {
+    return {cpx(0, -1), std::string("z")};
+  }
+  else if (a == "z" and b == "x")
+  {
+    return {cpx(0, 1), std::string("y")};
+  }
+  else if (a == "x" and b == "z")
+  {
+    return {cpx(0, -1), std::string("y")};
+  }
+  else if (a == "y" and b == "z")
+  {
+    return {cpx(0, 1), std::string("x")};
+  }
+  else if (a == "z" and b == "y")
+  {
+    return {cpx(0, -1), std::string("x")};
+  }
+  else
+  {
+    std::cout << "error commutators " << std::endl;
+    return {cpx(0, 0), std::string("0")};
+  }
+}
+std::pair<cpx, op_vec> run_loop(op_vec op)
+{
+  cpx pref = 1;
+  std::map<int, spin_op> copied;
+  for (int i = 0; i < op.size(); i++)
+  {
+    copied.insert({i, op[i]});
+  }
+  for (int i = 0; i < op.size() - 1; i++)
+  {
+    auto it = copied.find(i);
+
+    if (it != copied.end())
     {
-
-      auto [new_coeff, new_op]=run_loop(new_list);
-      pref*=new_coeff;
-      if(new_op.size()==0 or new_op==new_list)
-	{
-	  new_list=new_op;
-	   change=true;}
-      else{
-	new_list=new_op;
-       }
-      
+      auto o1 = copied[i];
+      auto o2 = copied[i + 1];
+      if (o1.pos() != o2.pos())
+      {
+        continue;
+      }
+      else if (o1.dir_ == o2.dir_)
+      {
+        copied.erase(i);
+        copied.erase(i + 1);
+      }
+      else
+      {
+        auto [new_coeff, new_op] = get_dir(o1.dir_, o2.dir_);
+        copied.erase(i);
+        copied.erase(i + 1);
+        copied.insert({i, spin_op(new_op, o1.site_, o1.offset_)});
+        pref *= new_coeff;
+      }
     }
-  return std::pair<cpx,op_vec>{pref, new_list};
- }
+  }
 
+  op_vec final_vec;
+  for (auto it = copied.begin(); it != copied.end(); ++it)
+  {
+    final_vec.push_back(it->second);
+  }
 
-// double sort(op_vec& op, int n)
-// {
-//   sort(op.begin(), op.end(),[](spin_op o1, spin_op o2){return o1.pos()<o2.pos();} );
-//  return 1;
-// }
-   std::vector<std::pair<cpx,op_vec>> generate_all_terms(op_vec op, bool& unit_found)
- {
-      std::vector<std::pair<cpx,op_vec>> terms;
-      // auto [fac, vec] get_normal_form(op)
-     // terms.push_back({fac,vec});
-     // unit_found=false;
-     return terms;
- }
-// std::pair<bool, double> check_constant(op_vec op, bool is_con)
-// {
-//   bool zero=false;
-//   for(int i=0; i<op.size()-1; i++)
-//     {
-//       if(op[i]==op[i+1])
-// 	{return true;}
-//     }
-    
-//   return zero;
+  return {pref, final_vec};
+}
 
+std::pair<cpx, op_vec> get_normal_form(op_vec op)
+{
+  if (op.size() < 1)
+  {
+    std::cout << "error: unit sent to normal form" << std::endl;
+  }
 
-// }
-//   std::vector<std::pair<double,op_vec>> terms;
-  
-//     terms.push_back({1.,op});
+  cpx pref(1., 0);
+  bool change = false;
+  auto new_list = op;
+  sort(new_list.begin(), new_list.end(), [](spin_op &o1, spin_op &o2)
+       { return o1.pos() < o2.pos(); });
+  for (auto a : new_list)
+  {
+    // std::cout<<a.pos()<<std::endl;
+  }
 
-//   // // bool ended=false;
-//   // // auto it = begin (terms);
-//   //  int ind=0;
-//   //  int size=terms.size();
-  
-//   //  while(ind<size)
-//   //    {
-//   //      auto n=check_one_term(terms[ind].second,terms[ind].first, terms, unit_found);
-//   //  terms.insert(terms.end(), n.begin(), n.end());
-  
-//   //  ind+=1;
+  while (not change)
+  {
 
-//   //  size=terms.size();
-    
-//   //    }
-//   //  // for(int i=0; i<terms;)
-//   //   // for(auto a: terms)
-//   //   //    {
-//   //   // 	 std::cout<< "coeff "<< a.first<< " term "<< print_op(a.second)<<std::endl;
-//   //   //    }
+    auto [new_coeff, new_op] = run_loop(new_list);
+    pref *= new_coeff;
+    if (new_op.size() == 0 or new_op == new_list)
+    {
+      new_list = new_op;
+      change = true;
+    }
+    else
+    {
+      new_list = new_op;
+    }
+  }
+  return std::pair<cpx, op_vec>{pref, new_list};
+}
 
-//   return terms;
+std::vector<std::pair<cpx, op_vec>> generate_all_terms(op_vec op, bool &unit_found)
+{
+  std::vector<std::pair<cpx, op_vec>> terms;
 
-// }
-
-
+  return terms;
+}
