29f91a7431cd57e36f41d60b343b8bd41d9085e7
David Jansen <jansen.david0712@gmail.com>
2025-11-12 20:27:07 +0100

    deleted unnecessary dependence

--- cpp/include/util.hpp
+++ cpp/include/util.hpp
@@ -5,61 +5,84 @@
 #include <cmath>
 #include "fusion.h"
 #include <bits/stdc++.h>
-#include"spins.hpp"
-#include<unordered_map>
+
+#include <unordered_map>
 #include <Eigen/Dense>
 #include <Eigen/Sparse>
-#include"definitions.hpp"
+#include "definitions.hpp"
 // defenitions
+using namespace mosek::fusion;
+using namespace monty;
 const double pi = std::acos(-1.0);
 //
-struct matrix_organizer{
+std::shared_ptr<ndarray<int, 1>> nint(const std::vector<int> &X) { return new_array_ptr<int>(X); }
+std::shared_ptr<ndarray<double, 1>> ndou(const std::vector<double> &X) { return new_array_ptr<double>(X); }
+
+template <class T>
+int getIndex(std::vector<T> v, T K)
+{
+  auto it = find(v.begin(), v.end(), K);
+
+  // If element was found
+  if (it != v.end())
+  {
+
+    // calculating the index
+    // of K
+    int index = it - v.begin();
+    return index;
+  }
+  else
+  {
+    // If the element is not
+    // present in the vector
+    return -1;
+  }
+}
+struct matrix_organizer
+{
   std::vector<int_pair> matrix_positions;
   std::vector<double> matrix_values;
   std::vector<double> b;
   int variable_index{0};
-  bool has_elements_=false;
-  
-  void add_values(int_pair position,double value)
-  {
-    has_elements_=true;
+  bool has_elements_ = false;
 
-    auto index=getIndex(matrix_positions, position);
-    
-    
+  void add_values(int_pair position, double value)
+  {
+    has_elements_ = true;
+
+    auto index = getIndex(matrix_positions, position);
+
     // Check if the target value was found
-    if(index<0)
-      {
-	matrix_positions.push_back(position);
-	matrix_values.push_back(value);
-	
-      }
+    if (index < 0)
+    {
+      matrix_positions.push_back(position);
+      matrix_values.push_back(value);
+    }
     else
-      {
-	matrix_values[index]+=value;
-      }
-    
+    {
+      matrix_values[index] += value;
+    }
   }
   Matrix::t make_matrix(int dim1, int dim2)
   {
     std::vector<int> rows;
     std::vector<int> cols;
     std::vector<double> T;
-    int i=0;
-    for(auto& p:matrix_positions )
+    int i = 0;
+    for (auto &p : matrix_positions)
+    {
+      if (std::abs(matrix_values[i]) > 1e-9)
       {
-        if(std::abs(matrix_values[i])>1e-9)
-        {
-	rows.push_back(p.first);
-	cols.push_back(p.second);
-	T.push_back(matrix_values[i]);
-        }
-  //std::cout<< "values "<<matrix_values[i]<<std::endl;
-
-	i++;
+        rows.push_back(p.first);
+        cols.push_back(p.second);
+        T.push_back(matrix_values[i]);
       }
-    
-  
-  return Matrix::sparse(dim1, dim2, nint(rows), nint(cols), ndou(T));
+      // std::cout<< "values "<<matrix_values[i]<<std::endl;
+
+      i++;
+    }
+
+    return Matrix::sparse(dim1, dim2, nint(rows), nint(cols), ndou(T));
   }
 };
