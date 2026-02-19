1743f10aa35dfdb903c829fa713723605f6e7bb2
David Jansen <jansen.david0712@gmail.com>
2026-02-19 22:22:05 +0100

    fixed sorting bug

--- cpp/include/spins.hpp
+++ cpp/include/spins.hpp
@@ -315,8 +315,14 @@ std::pair<cpx, op_vec> get_normal_form(op_vec op)
   cpx pref(1., 0);
   bool change = false;
   auto new_list = op;
-  sort(new_list.begin(), new_list.end(), [](spin_op &o1, spin_op &o2)
-       { return o1.pos() < o2.pos(); });
+  // deleted
+  //  sort(new_list.begin(), new_list.end(), [](spin_op &o1, spin_op &o2)
+  //       { return o1.pos() < o2.pos(); });
+  std::stable_sort(new_list.begin(), new_list.end(),
+                   [](const spin_op &lhs, const spin_op &rhs)
+                   {
+                     return lhs.pos() < rhs.pos();
+                   });
   for (auto a : new_list)
   {
     // std::cout<<a.pos()<<std::endl;
