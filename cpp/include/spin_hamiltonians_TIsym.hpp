22d8cb2b29b64edea07c452359e46afc4cb6e5c5
David Jansen <jansen.david0712@gmail.com>
2026-05-14 13:15:27 +0200

    added a global 2d magnetization

--- cpp/include/spin_hamiltonians_TIsym.hpp
+++ cpp/include/spin_hamiltonians_TIsym.hpp
@@ -629,27 +629,72 @@ std::vector<double> define_TFI_1d_sos(std::map<std::string, int> refs, Lattice l
       return vals;
 }
 template<typename Lattice>
-std::vector<double> define_magnetization_sos(std::map<std::string, int> refs, Lattice lattice, std::string term)
+std::vector<double> define_magnetization_sos(Lattice lattice, std::string term)
 {
-
+      std::vector<string_pair> dirs{string_pair("x", "x"), string_pair("z", "z"), string_pair("y", "y")};
+      std::vector<double> vals(lattice.variable_map_.size(), 0);
       auto offset_vector = lattice.get_offset_vec();
-      std::vector<double> vals(refs.size(), 0);
-      op_vec v_p = {spin_op(term, {0, 0}, offset_vector)};
 
-      auto [fac_p, nf_p] = get_normal_form(v_p);
+            op_vec v_p = {spin_op(term, {0, 0}, offset_vector)};
+            // auto [fac_p, nf_p] = get_normal_form(v_p);
 
-      auto [key_p, coeff_map_p] = lattice.TI_map_.at(key_dir_pos(nf_p));
+            auto [key_p, coeff_map_p] = lattice.TI_map_.at(key_dir_pos(v_p));
 
-      auto el_p = refs.at(op_key_label(key_p));
+            assert(abs(coeff_map_p.imag()) < 1e-8);
 
-      if (std::abs(fac_p.imag()) > 1e-9 or std::abs(coeff_map_p.imag()) > 1e-9)
-      {
-            std::cout << "error: Hamiltonian contains complex elements " << std::endl;
-      }
-      {
+            auto el_p = lattice.variable_map_.at(op_key_label(key_p));
 
-            vals[el_p] += fac_p.real() * coeff_map_p.real();
-      }
+  
+
+                  vals[el_p] += coeff_map_p.real();
+          
+      //       op_vec v_t = {spin_op(term.first, {0, 0}, offset_vector), spin_op(term.second, {0, 1}, offset_vector)};
+      //       // auto [fac_t, nf_t] = get_normal_form(v_t);
+      //       auto [key_t, coeff_map_t] = lattice.TI_map_.at(key_dir_pos(v_t));
+      //       auto el_t = lattice.variable_map_.at(op_key_label(key_t));
+      //       coeff = J;
+      //       if (term == string_pair("z", "z"))
+      //       {
+      //             coeff = Delta;
+      //       }
+
+      //       // if (std::abs(fac_t.imag()) > 1e-9 or std::abs(coeff_map_t.imag()) > 1e-9)
+      //       // {
+      //       //       std::cout << "error: Hamiltonian contains complex elements " << std::endl;
+      //       // }
+
+      //       {
+
+      //             vals[el_t] += coeff * coeff_map_t.real() / 4.;
+      //       }
+      // }
 
       return vals;
 }
+
+
+// template<typename Lattice>
+// std::vector<double> define_magnetization_sos(std::map<std::string, int> refs, Lattice lattice, std::string term)
+// {
+
+//       auto offset_vector = lattice.get_offset_vec();
+//       std::vector<double> vals(refs.size(), 0);
+//       op_vec v_p = {spin_op(term, {0, 0}, offset_vector)};
+
+//       auto [fac_p, nf_p] = get_normal_form(v_p);
+
+//       auto [key_p, coeff_map_p] = lattice.TI_map_.at(key_dir_pos(nf_p));
+
+//       auto el_p = refs.at(op_key_label(key_p));
+
+//       if (std::abs(fac_p.imag()) > 1e-9 or std::abs(coeff_map_p.imag()) > 1e-9)
+//       {
+//             std::cout << "error: Hamiltonian contains complex elements " << std::endl;
+//       }
+//       {
+
+//             vals[el_p] += fac_p.real() * coeff_map_p.real();
+//       }
+
+//       return vals;
+// }
